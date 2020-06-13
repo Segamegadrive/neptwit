51395f2ccd5a8b2a0d6ad653dd2382c1f745551d
unknown <segamegadrive153@hotmail.com>
2020-06-13 11:27:06 +0100

    dummy dic, more html and css

--- neptwit_proj/neptwit_blog/views.py
+++ neptwit_proj/neptwit_blog/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Sagar',
+        'title': 'Sagar title',
+        'content': 'Sagar content',
+        'date_posted': '13 June 2020'
+    },
+    {
+        'author': 'Avana',
+        'title': 'Avana title',
+        'content': 'Avana content',
+        'date_posted': '14 June 2020'
+    }
+]
+
 
 def index(request):
-    return render(request, 'neptwit_blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'neptwit_blog/home.html', context)
 
 
 def about(request):
