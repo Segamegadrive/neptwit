d0bbb1d319d6490b46f1e9c9340e17eb8163e948
unknown <segamegadrive153@hotmail.com>
2020-06-16 23:35:35 +0100

    Twit post update - UpdateView class added

--- neptwit_proj/neptwit_blog/views.py
+++ neptwit_proj/neptwit_blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import (
     ListView,
     DetailView,
-    CreateView
+    CreateView,
+    UpdateView
 )
 from django.http import HttpResponse
 from .models import Post
@@ -28,10 +30,23 @@ class PostDetailView(DetailView):
     model = Post
 
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'content']
 
+    # To allow current logged in user to post new twit. If this method isn't defined, throws an IntegrityError
+    # Overriding form_valid method
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    # To allow current logged in user to post new twit. If this method isn't defined, throws an IntegrityError
+    # Overriding form_valid method
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
