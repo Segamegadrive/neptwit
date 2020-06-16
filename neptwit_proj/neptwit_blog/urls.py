b25e133dd763197ae018467a8f0070413ac6c14a
unknown <segamegadrive153@hotmail.com>
2020-06-16 16:45:51 +0100

    Class based views added

--- neptwit_proj/neptwit_blog/urls.py
+++ neptwit_proj/neptwit_blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from .views import PostListView, PostDetailView
 from . import views
 
 
 # Name arg is given on the path to differentiate from other app which have similar names
 urlpatterns = [
-    path('', views.index, name='neptwitblog-home'),
+    path('', PostListView.as_view(), name='neptwitblog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('about/', views.about, name='neptwitblog-about'),
 ]
--- neptwit_proj/neptwit_blog/views.py
+++ neptwit_proj/neptwit_blog/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from django.http import HttpResponse
 from .models import Post
 
 
+# function based views/controller
 def index(request):
     context = {
         'posts': Post.objects.all()
     }
     return render(request, 'neptwit_blog/home.html', context)
+
+
+# Class based view
+class PostListView(ListView):
+    model = Post
+    template_name = 'neptwit_blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
 
 
 def about(request):
